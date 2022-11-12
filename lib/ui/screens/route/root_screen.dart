878638db30976ea337bc279880c4d12f8880b44c
Mr.mojtaba <mr.mojtaba47@gmail.com>
2022-11-12 16:50:09 +0330

    fix: bottomNavigation

--- lib/ui/screens/route/root_screen.dart
+++ lib/ui/screens/route/root_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cup_coffee/data/utils/m_extension.dart';
 import 'package:cup_coffee/generated/assets.dart';
+import 'package:cup_coffee/ui/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,17 +13,61 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
+  int _currentBottomNavigation = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(color: Colors.green),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => setState(() {
+          _currentBottomNavigation = value;
+        }),
+        currentIndex: _currentBottomNavigation,
+        enableFeedback: true,
+        elevation: 0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.black,
+        selectedItemColor: assetsColor,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.shifting,
         items: [
-          BottomNavigationBarItem(icon: SvgPicture.asset(Assets.iconsHome),label: ''),
-          BottomNavigationBarItem(icon: SvgPicture.asset(Assets.iconsShoppingBag),label: ''),
-          BottomNavigationBarItem(icon: SvgPicture.asset(Assets.iconsMapPin),label: ''),
-          BottomNavigationBarItem(icon: SvgPicture.asset(Assets.iconsHeart),label: ''),
-          BottomNavigationBarItem(icon: SvgPicture.asset(Assets.iconsUser),label: ''),
+          BottomNavigationBarItem(
+              activeIcon: SvgPicture.asset(
+                Assets.iconsHome,
+                color: assetsColor,
+              ),
+              icon: SvgPicture.asset(Assets.iconsHome),
+              label: ''),
+          BottomNavigationBarItem(
+              activeIcon: SvgPicture.asset(
+                Assets.iconsShoppingBag,
+                color: assetsColor,
+              ),
+              icon: SvgPicture.asset(Assets.iconsShoppingBag),
+              label: ''),
+          BottomNavigationBarItem(
+              activeIcon: SvgPicture.asset(
+                Assets.iconsMapPin,
+                color: assetsColor,
+              ),
+              icon: SvgPicture.asset(Assets.iconsMapPin),
+              label: ''),
+          BottomNavigationBarItem(
+              activeIcon: SvgPicture.asset(
+                Assets.iconsHeart,
+                color: assetsColor,
+              ),
+              icon: SvgPicture.asset(Assets.iconsHeart),
+              label: ''),
+          BottomNavigationBarItem(
+              activeIcon: SvgPicture.asset(
+                Assets.iconsUser,
+                color: assetsColor,
+              ),
+              icon: SvgPicture.asset(Assets.iconsUser),
+              label: ''),
         ],
       ),
     );
--- lib/ui/utils/colors.dart
+++ lib/ui/utils/colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color.fromRGBO(255, 176, 103, 1);
-Color assetsColor = Color.fromRGBO(49, 77, 69, 1);
-Color backgroundPrimaryColor = Color.fromRGBO(255, 176, 103, 0.1);
-Color textGray = Color.fromRGBO(157, 157, 157, 1);
-
+Color primaryColor = const Color.fromRGBO(255, 176, 103, 1);
+Color assetsColor = const Color.fromRGBO(49, 77, 69, 1);
+Color backgroundPrimaryColor = const Color.fromRGBO(255, 176, 103, 0.1);
+Color textGray = const Color.fromRGBO(157, 157, 157, 1);
